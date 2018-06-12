156f9842ad993b06baba6b9ddf35fece6447350c
Unknown <ariel.ec84@gmail.com>
2018-06-12 16:53:04 -0400

    Before Cleanup

--- Hackathon2018/Assets/Scripts/Spawner.cs
+++ Hackathon2018/Assets/Scripts/Spawner.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+	public enum Team
+	{
+		TeamA, 
+		TeamB,
+	}
+
+	public enum Direction
+	{
+		back,
+		forward,
+	}
+
+	public Team TeamWeeble;
+	public Direction WeebleDirection;
 	public GameObject MeleeWeeblePrefab;
 	public GameObject HealerWeeblePrefab;
 	public GameObject TankWeeblePrefab;
@@ -13,47 +27,45 @@ public class Spawner : MonoBehaviour
 	public Transform Location3;
 	public Transform Location4;
 	public Transform Location5;
-	public Transform Location6;
-	public Transform Location7;
-	public Transform Location8;
-	public Transform Location9;
-	public Transform Location10;
-	
 	
 	// Use this for initialization
 	void Start ()
 	{
+		InstantiateWeeble(MeleeWeeblePrefab, Location1);
 		GameObject weeble1 = Instantiate(MeleeWeeblePrefab);
 		weeble1.transform.position = Location1.position;
+		WeebleMover mover1 = weeble1.GetComponent<WeebleMover>();
+		mover1.Init(TeamWeeble, WeebleDirection);
 
 		GameObject weeble2 = Instantiate(HealerWeeblePrefab);
 		weeble2.transform.position = Location2.position;
+		WeebleMover mover2 = weeble2.GetComponent<WeebleMover>();
+		mover2.Init(TeamWeeble, WeebleDirection);
 		
 		GameObject weeble3 = Instantiate(TankWeeblePrefab);
 		weeble3.transform.position = Location3.position;
+		WeebleMover mover3 = weeble3.GetComponent<WeebleMover>();
+		mover3.Init(TeamWeeble,WeebleDirection);
 		
 		GameObject weeble4 = Instantiate(MeleeWeeblePrefab);
 		weeble4.transform.position = Location4.position;
+		WeebleMover mover4 = weeble4.GetComponent<WeebleMover>();
+		mover4.Init(TeamWeeble, WeebleDirection);
 		
 		GameObject weeble5 = Instantiate(RangedWeeblePrefab);
 		weeble5.transform.position = Location5.position;
-
-		GameObject weeble6 = Instantiate(MeleeWeeblePrefab);
-		weeble6.transform.position = Location6.position;
-
-		GameObject weeble7 = Instantiate(HealerWeeblePrefab);
-		weeble7.transform.position = Location7.position;
-
-		GameObject weeble8 = Instantiate(TankWeeblePrefab);
-		weeble8.transform.position = Location8.position;
-
-		GameObject weeble9 = Instantiate(MeleeWeeblePrefab);
-		weeble9.transform.position = Location9.position;
-
-		GameObject weeble10 = Instantiate(RangedWeeblePrefab);
-		weeble10.transform.position = Location10.position;
+		WeebleMover mover5 = weeble5.GetComponent<WeebleMover>();
+		mover5.Init(TeamWeeble, WeebleDirection);
 	}
-	
+
+	private void InstantiateWeeble(GameObject prefab, Transform location)
+	{
+		GameObject weeble = Instantiate(prefab);
+		weeble.transform.position = location.position;
+		WeebleMover mover = weeble.GetComponent<WeebleMover>();
+		mover.Init(TeamWeeble, WeebleDirection);
+
+	}
 	// Update is called once per frame
 	void Update () {
 		
---
+++ Hackathon2018/Assets/Scripts/WeebleMover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeebleMover : MonoBehaviour
+{
+	private Vector3 _direction;
+	private Spawner.Team _team;
+	private Vector3 _startPosition; 
+	// Use this for initialization
+	void Start ()
+	{
+		_startPosition = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 traveled = transform.position - _startPosition;
+		float Distance = traveled.magnitude;
+		
+		if (Distance < 10)
+		{
+			
+			var dt = Time.deltaTime;
+			var velocity = 1.0f;
+		
+			transform.position = transform.position + (dt * velocity * _direction);
+
+		}
+		else
+		{
+			
+		}
+	}
+
+	public void Init(Spawner.Team team, Spawner.Direction direction)
+	{
+		if (direction == Spawner.Direction.back)
+		{
+			_direction = Vector3.back;
+		}
+		else
+		{
+			_direction = Vector3.forward;
+		}
+
+		_team = team;
+	}
+}
