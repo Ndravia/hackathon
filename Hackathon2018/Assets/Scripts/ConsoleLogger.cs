d615808fa6d50fd5520b433b12a4006d3d5b9634
Unknown <ariel.ec84@gmail.com>
2018-06-14 15:32:28 -0400

    Attacking!

---
+++ Hackathon2018/Assets/Scripts/ConsoleLogger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ConsoleLogger: MonoBehaviour
+{
+   private void Start()
+   {
+      int a = 2;
+      int b = 3;
+      int c = 4;
+      int d = 1;
+      
+      int i = CombineTwo(3,4);
+      Debug.Log(i);
+      int o = SubtractOne(6,3);
+      Debug.Log(o);
+      int q = CombineTwo(a,b);
+      int s = CombineTwo(c,d);
+      int m = SubtractOne(q,s);
+      Debug.Log(m);
+   }
+
+   private int SubtractOne(int num1, int num2)
+   {
+      int p = num1 - num2;
+      return p;
+   }   
+   
+   private int CombineTwo(int num1, int num2)
+   {
+      int w = num1 + num2;
+      return w;
+   }
+}
--- Hackathon2018/Assets/Scripts/WeebleMover.cs
+++ Hackathon2018/Assets/Scripts/WeebleMover.cs
@@ -1,25 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class WeebleMover : MonoBehaviour
 {
+	public float velocity = 1.0f;
+	public int Health = 100;
+	public int Damage = 1;
+	public float CoolDown = 1.5f;
+	public bool IsDead = false;
+		
+	private WeebleMover _enemyWeeble;
 	private Vector3 _direction;
+	private float _timer;
 
 	private void OnTriggerEnter(Collider other)
 	{
-		_direction = Vector3.zero;
-	}
+		_enemyWeeble = other.transform.parent.gameObject.GetComponent<WeebleMover>();
+	}	
 
 	// Update is called once per frame
 	void Update ()
-	{		
+	{
 		var dt = Time.deltaTime;
-		var velocity = 1.0f;
 	
-		transform.position = transform.position + (dt * velocity * _direction);
+		if (_enemyWeeble != null)
+		{
+			//do damage
+			if (_timer > CoolDown)
+			{
+				_enemyWeeble.TakeDamage(Damage);
+				_timer = 0;
+				if (_enemyWeeble.IsDead)
+				{
+					_enemyWeeble = null;
+				}
+			}
+			else
+			{
+				_timer = _timer + dt;
+			}
+		}
+		else
+		{
+			transform.position = transform.position + (dt * velocity * _direction);
+		}
+
+		if (IsDead)
+		{
+			Destroy(gameObject);
+		}
 	}
 
+	public void TakeDamage(int damagetaken)
+	{
+		Health = Health - damagetaken;
+		if (Health <= 0)
+		{
+			IsDead = true;
+			Debug.Log("A Weeble Has Died!");
+		}
+	}
+	
 	public void Init(Spawner.Direction d)
 	{
 		if (d == Spawner.Direction.back)
